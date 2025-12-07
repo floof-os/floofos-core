03eb382fbde9fe1b8073a54c6a898c2bd3361497
Irsyad1609 <irsyadvekok@gmail.com>
2025-12-07 13:28:03 +0100

    hide log from kernel log

--- etc/profile.d/floof-banner.sh
+++ etc/profile.d/floof-banner.sh
@@ -6,5 +6,4 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License.
-echo "FloofOS [Fast Line-rate Offload On Fabric Operating System]"
-echo "version: FloofOS 1.0"
+echo ""
