d171ce9eac730c93e6c55765aac5b0e7f22c8e95
Irsyad1609 <irsyadvekok@gmail.com>
2025-12-14 12:02:05 +0100

    update banner

--- etc/profile.d/floof-banner.sh
+++ etc/profile.d/floof-banner.sh
@@ -6,4 +6,4 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License.
-echo "FloofOS 1.0"
+echo ""
